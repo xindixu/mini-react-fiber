229ea4a4e9a42e645f700d1490c29347a4084c69
Xindi Xu <xindixu@utexas.edu>
2020-10-09 21:09:22 -0500

    fiber

--- src/reactjs/index.js
+++ src/reactjs/index.js
@@ -21,7 +21,111 @@ function createElement(type, props, ...children) {
   };
 }
 
+// `render` will initiate first task
+let nextUnitOfWork = null;
+// manger `diff` tasks
+function workLoop(deadline) {
+  // current tick hasn't ended
+  while (nextUnitOfWork && deadline.timeRemaining() > 1) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+  }
+  window.requestIdleCallback(workLoop);
+}
+
+/*
+ * fiber:
+ * Parent -> First child
+ * Any child -> Parent
+ * First child -> second child -> third child
+ */
+
+function performUnitOfWork(fiber) {
+  // get next task
+
+  if (!fiber.dom) {
+    fiber.dom = creatDom(fiber);
+  }
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+  const elements = fiber.props.children;
+  // construct fiber from vdom
+
+  /*
+   * fiber = {
+   *  dom,
+   *  parent,
+   *  child,
+   *  siblings
+   * }
+   */
+
+  let index = 0;
+  let prevSibling = null;
+
+  while (index < elements.length) {
+    const element = elements[index];
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.prevSibling = newFiber;
+    }
+    index += 1;
+    prevSibling = fiber;
+
+    // find next unit of work
+    // 1. child
+    // 2. sibling
+    // 3. parent
+    if (fiber.child) {
+      return fiber.child;
+    }
+
+    let nextFiber = fiber;
+    while (nextFiber) {
+      if (nextFiber.sibling) {
+        return nextFiber.sibling;
+      }
+      nextFiber = newFiber.parent;
+    }
+  }
+}
+
 function render(vdom, container) {
+  // const dom =
+  //   vdom.type === "text"
+  //     ? document.createTextNode(vdom.nodeValue)
+  //     : document.createElement(vdom.type);
+
+  // Object.keys(vdom.props)
+  //   .filter((key) => key !== "children")
+  //   .forEach((name) => {
+  //     // TODO: event listeners
+  //     dom[name] = vdom.props[name];
+  //   });
+
+  // When num of children gets big, this will cause issue since we cannot pause this loop.
+  // vdom.props.children.forEach((child) => {
+  //   render(child, dom);
+  // });
+
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [vdom],
+    },
+  };
+  // container.appendChild(dom);
+}
+
+function creatDom(vdom) {
   const dom =
     vdom.type === "text"
       ? document.createTextNode(vdom.nodeValue)
@@ -33,32 +137,11 @@ function render(vdom, container) {
       // TODO: event listeners
       dom[name] = vdom.props[name];
     });
-
-  // When num of children gets big, this will cause issue since we cannot pause this loop.
-  vdom.props.children.forEach((child) => {
-    render(child, dom);
-  });
-
-  container.appendChild(dom);
-}
-
-// `render` will initiate first task
-let nextUnitOfWork = null;
-// manger `diff` tasks
-function workLoop(deadline) {
-  // current tick hasn't ended
-  while (nextUnitOfWork && deadline.timeRemaining() > 1) {
-    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
-  }
-  requestIdleCallback(workLoop);
-}
-
-function performUnitOfWork(fiber) {
-  // get next task
+  return dom;
 }
 
 // ref: https://developer.mozilla.org/en-US/docs/Web/API/Window/requestIdleCallback
 // Process tasks when the event loop is idle
-requestIdleCallback(workLoop);
+window.requestIdleCallback(workLoop);
 
 export default { createElement, render };
